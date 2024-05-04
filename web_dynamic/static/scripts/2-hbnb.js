beabfa83a31dcf67f9b243c4ad6754221fbd0c08
Sobilo34 <bilalsolih60@gmail.com>
2024-05-04 18:02:27 +0100

    updated files to fufil task 2 requirements

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
     // initialising the list var
     let selected = {};
-    $(input[type='checkbox']).$().change(function () { 
+    
+    $('input[type="checkbox"]').change(function() {
         if ($(this).is(":checked")) {
-            selected[$(this).atrr('data-id')] = $(this).atrr('data_name');
+            selected[$(this).attr('data-id')] = $(this).attr('data-name');
         } else {
-            delete selected[$(this).atrr('data-id')];
+            delete selected[$(this).attr('data-id')];
         }
-        $('.amenities h4').text(Object.values(amenities).join(', '));
+        $('.amenities h4').text(Object.values(selected).join(', '));
     });
-})
+
+    // Fetch the status of the API
+    let url = "http://0.0.0.0:5001/api/v1/status/"
+
+    $.get(url, function (response, status) {
+        console.log(response)
+        console.log(status)
+        if (status === "success") {
+            console.log(response)
+        }
+    })
+});
+
